4d2ecabfb6742185f47b04ddbf0adf2c827da410
Vinícius Barbosa <alencarvi@gmail.com>
2019-09-17 03:46:30 -0300

    Alterações no controller para integração com frontend

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const routes = require('./src/routes')
 const app = express ();
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }))
 
 app.use(routes);
 
--- src/controllers/MovieController.js
+++ src/controllers/MovieController.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-const axios = require('axios');
+const fetch = require('node-fetch');
 
 dotenv.config()
 const { API_URL, API_KEY } = process.env
@@ -9,35 +9,39 @@ module.exports = {
         const { page } = req.query
         const url = `${API_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=${page}`;
 
-        let response = await axios.get(url);
+        let response = await fetch(url);
+        let data = await response.json();
 
-        return res.send(response.data.results)
+        return res.send(data.results)
     },
 
     async details(req, res) {
         const { id } = req.params;
         const url = `${API_URL}/movie/${id}?api_key=${API_KEY}&language=en-US`;
 
-        let response = await axios.get(url);
+        let response = await fetch(url);
+        let data = await response.json();
 
-        return res.send(response.data);
+        return res.send(data);
     },
 
     async genres(req, res) {
         const url = `${API_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`;
 
-        let response = await axios.get(url);
+        let response = await fetch(url);
+        let data = await response.json();
 
-        return res.send(response.data);
+        return res.send(data.genres);
     },
 
     async search(req, res) {
         const { query, page } = req.query
         const url = `${API_URL}/search/movie?api_key=${API_KEY}&query=${query}&page=${page}&language=en-US`;
 
-        let response = await axios.get(url);
+        let response = await fetch(url);
+        let data = await response.json();
 
-        return res.send(response.data);
+        return res.send(data.results);
     }
 
 }
